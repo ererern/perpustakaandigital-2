373b801b70b1131311cbc437fc2a598cd0f3ee8c
Marsya ratulia <142872856+ererern@users.noreply.github.com>
2024-03-03 00:23:22 +0700

    Update databuku.php

--- auth/databuku.php
+++ auth/databuku.php
@@ -238,7 +238,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h4 class="modal-title">Pinjam Buku</h4>
+                            <h4 class="modal-title">Ulasan Buku</h4>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
